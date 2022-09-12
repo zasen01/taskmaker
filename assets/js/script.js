1f21b8f1448094bd0a6b8530f58a9d083f64973f
Myeisha Swain <zse0296@gmail.com>
2022-09-12 15:27:10 -0400

    polisihing JS to add task to list

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,24 @@ var taskToDoEl = document.querySelector("#task-to-do");
 
 
 
-var createTaskHandler = function() {
-    
-    event.preventDefault();
+var createTaskHandler = function(event) {
 
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskNameInput);
+
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new Task.";
+
+    //div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    //adding HTML content to DIV
+    taskInfoEl.innerHTML = " <h3 class='task-name'> " + taskNameInput + "</h3><span class'task-type'> " + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
     taskToDoEl.appendChild(listItemEl);
 }
 
